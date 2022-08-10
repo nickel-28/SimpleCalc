c0d34def133bb1e41cda13cd72716ecce85b28f9
nickel-28 <nikhilraj875727@gmail.com>
2022-08-10 19:41:44 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:calculator_app/SimpleCalculator.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primaryColor: Colors.amber),
+      home: SimpleCalculator(),
+    );
+  }
+}
